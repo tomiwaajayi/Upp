1ef67e57aa23f1af1a73b6f72964a5d0d4a0b8be
mhizterkeyz <38281402+mhizterkeyz@users.noreply.github.com>
2022-09-29 14:18:15 +0100

    add authentication event

--- events/authentication.events.ts
+++ events/authentication.events.ts
@@ -2,4 +2,5 @@ import {SERVICES} from '../constants/services.constant';
 
 export const AUTHENTICATION_EVENTS = {
   findOrCreateEmployeeUser: `${SERVICES.AUTHENTICATION_SERVICE}.user.findOrCreateEmployeeUser`,
+  switchCurrentUserOrganization: `${SERVICES.AUTHENTICATION_SERVICE}.user.switchCurrentUserOrganization`,
 };
