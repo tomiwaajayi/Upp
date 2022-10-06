7c4da680edcf8696a2bb29d62c9a249c39227a86
shonubijerry <korede@verifi.ng>
2022-10-07 00:04:50 +0100

    add organization interface

--- interfaces/account/organization.interface.ts
+++ interfaces/account/organization.interface.ts
@@ -1,5 +1,22 @@
+import {BaseSchemaInterface} from '../base.interface';
+
 export interface IOrganizationResponse {
   name: string;
   email: string;
   id: string;
+}
+
+export interface Organization {
+  country: string;
+  email: string;
+  name: string;
+  logo?: string;
+  address?: string;
+  industry?: string;
+  website?: string;
+  registrationNumber?: string;
+  registrationDate?: Date;
+  createdBy: string;
+  isDeleted?: boolean;
+  deletedAt?: Date;
 }
