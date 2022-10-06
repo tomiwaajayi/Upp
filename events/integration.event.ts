5422994115249f1c95e6c75e8654a9581cc28c47
johnny evans <evanseburu32@gmail.com>
2022-10-06 01:54:42 +0100

    integration events and terminate emps

--- events/integration.event.ts
+++ events/integration.event.ts
@@ -2,9 +2,7 @@ import {SERVICES} from '../constants/services.constant';
 export const PEOPLE = 'people';
 export const CDB = 'cdb';
 
-
 export const INTEGRATION_EVENTS = {
-    deleteEmployeeOnPeople: `${SERVICES.INTEGRATION_SERVICE}.${PEOPLE}.deleteEmployee`,
-    terminateEmployeeOnCDB: `${SERVICES.INTEGRATION_SERVICE}.${CDB}.terminateEmployee`,
-  };
-  
+  deleteEmployeeOnPeople: `${SERVICES.INTEGRATION_SERVICE}.${PEOPLE}.deleteEmployee`,
+  terminateEmployeeOnCDB: `${SERVICES.INTEGRATION_SERVICE}.${CDB}.terminateEmployee`,
+};
