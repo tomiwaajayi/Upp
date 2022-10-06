61f157bcd32f1d374705f55946abf09360105bc2
shonubijerry <korede@verifi.ng>
2022-10-07 00:07:45 +0100

    update

--- src/interfaces/account/organization.interface.d.ts
+++ src/interfaces/account/organization.interface.d.ts
@@ -3,3 +3,17 @@ export interface IOrganizationResponse {
     email: string;
     id: string;
 }
+export interface Organization {
+    country: string;
+    email: string;
+    name: string;
+    logo?: string;
+    address?: string;
+    industry?: string;
+    website?: string;
+    registrationNumber?: string;
+    registrationDate?: Date;
+    createdBy: string;
+    isDeleted?: boolean;
+    deletedAt?: Date;
+}
