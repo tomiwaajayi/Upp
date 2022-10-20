eb44efcff8a596e2ac73fee26df06e1721d6612c
mhizterkeyz <38281402+mhizterkeyz@users.noreply.github.com>
2022-10-20 09:09:56 +0100

    update authentication events

--- events/authentication.events.ts
+++ events/authentication.events.ts
@@ -4,4 +4,5 @@ export const AUTHENTICATION_EVENTS = {
   findOrCreateEmployeeUser: `${SERVICES.AUTHENTICATION_SERVICE}.user.findOrCreateEmployeeUser`,
   switchCurrentUserOrganization: `${SERVICES.AUTHENTICATION_SERVICE}.user.switchCurrentUserOrganization`,
   createUserOrganization: `${SERVICES.AUTHENTICATION_SERVICE}.user.createUserOrganization`,
+  sendEmailValidationToken: `${SERVICES.AUTHENTICATION_SERVICE}.user.sendEmailValidationToken`,
 };
