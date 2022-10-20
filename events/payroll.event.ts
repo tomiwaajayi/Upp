f66916b2f1bcd257fe0f54b75b8f4d782bdf765c
mhizterkeyz <38281402+mhizterkeyz@users.noreply.github.com>
2022-10-20 05:25:47 +0100

    update payroll events

--- events/payroll.event.ts
+++ events/payroll.event.ts
@@ -1,10 +1,13 @@
 import {SERVICES} from '../constants/services.constant';
 export const WORKSHEET = 'worksheet';
 export const PAYROLL = 'payroll';
+export const ORGANIZATION = 'organization';
 
 export const PAYROLL_EVENTS = {
   fetchWorksheet: `${SERVICES.PAYROLL_SERVICE}.${WORKSHEET}.fetchWorksheet`,
   updateWorksheet: `${SERVICES.PAYROLL_SERVICE}.${WORKSHEET}.updateWorksheet`,
   deleteWorksheet: `${SERVICES.PAYROLL_SERVICE}.${WORKSHEET}.deleteWorksheet`,
   removeEmployeeFromWorksheet: `${SERVICES.PAYROLL_SERVICE}.${WORKSHEET}.removeEmployeeFromWorksheet`,
+  getOrganizationSetting: `${SERVICES.PAYROLL_SERVICE}.${ORGANIZATION}.getSettings`,
+  updateOrganizationSetting: `${SERVICES.PAYROLL_SERVICE}.${ORGANIZATION}.updateSettings`,
 };
