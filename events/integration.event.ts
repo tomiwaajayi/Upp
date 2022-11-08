4aa5399a4e555421fb762a2f30def9169d2f569f
johnny evans <evanseburu32@gmail.com>
2022-11-08 19:07:21 +0100

    relayer

--- events/integration.event.ts
+++ events/integration.event.ts
@@ -1,11 +1,11 @@
 import {SERVICES} from '../constants/services.constant';
 export const PEOPLE = 'people';
 export const CDB = 'cdb';
-export const PRODUCT = 'product'
+export const PRODUCT = 'product';
 
 export const INTEGRATION_EVENTS = {
   deleteEmployeeOnPeople: `${SERVICES.INTEGRATION_SERVICE}.${PEOPLE}.deleteEmployee`,
   terminateEmployeeOnCDB: `${SERVICES.INTEGRATION_SERVICE}.${CDB}.terminateEmployee`,
   restoreEmployeeOnPeople: `${SERVICES.INTEGRATION_SERVICE}.${PEOPLE}.restoreEmployee`,
-  productProxyRelayer: `${SERVICES.INTEGRATION_SERVICE}.${PRODUCT}.proxyRelayer`
+  productProxyRelayer: `${SERVICES.INTEGRATION_SERVICE}.${PRODUCT}.proxyRelayer`,
 };
